e1ccac7bb18521567f90963fefbe082565ebbcfb
thebigpeng <13258118132@163.com>
2022-09-19 00:53:16 +0800

    新建单体应用项目

---
+++ miniweibo-spring/miniweibo/src/main/java/com/example/miniweibo/MiniweiboApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniweibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniweiboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniweiboApplication.class, args);
+    }
+
+}
---
+++ miniweibo-spring/miniweibo/src/test/java/com/example/miniweibo/MiniweiboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniweibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniweiboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
